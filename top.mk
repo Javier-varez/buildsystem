424f0aaf6fb0f910ef518587732af2d2663b59af
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2019-12-15 01:05:46 +0100

    Build shared and static libraries and link them to binaries

--- top.mk
+++ top.mk
@@ -1,8 +1,12 @@
+BUILD_SYSTEM_DIR        ?= .
 BUILD_DIR               ?= build
 BUILD_TARGET_DIR        := $(BUILD_DIR)/targets
 BUILD_INTERMEDIATES_DIR := $(BUILD_DIR)/intermediates
+BUILD_LIBS_DIR          := $(BUILD_DIR)/lib
 
 BUILD_BINARY            := $(BUILD_SYSTEM_DIR)/build_binary.mk
+BUILD_SHARED_LIB        := $(BUILD_SYSTEM_DIR)/build_shared_lib.mk
+BUILD_STATIC_LIB        := $(BUILD_SYSTEM_DIR)/build_static_lib.mk
 CLEAR_VARS              := $(BUILD_SYSTEM_DIR)/clear_vars.mk
 
 include $(BUILD_SYSTEM_DIR)/definitions.mk
@@ -11,7 +15,9 @@ SILENT                  := @
 MKDIR                   := $(SILENT)mkdir -p
 ECHO                    := $(SILENT)echo
 RM                      := $(SILENT)rm
+CP                      := $(SILENT)cp
 
+#default rule
 all:
 
 clean:
