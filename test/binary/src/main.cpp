a074b5a295ae2fe4c8d76f8369c04ed436a7b867
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-01-04 23:34:30 +0100

    Create shortcuts for run and clean
    Support cleaning and running individual targets
    
    Close #4

--- test/binary/src/main.cpp
+++ test/binary/src/main.cpp
@@ -2,8 +2,11 @@
 #include "static_exported.h"
 #include "shared_exported.h"
 
+#include <iostream>
+
 int main() {
   bool ok = (static_function() == 123456) &&
             (shared_function() == 1234);
+  std::cout << "OK: " << ok << std::endl;
   return !ok;
 }
